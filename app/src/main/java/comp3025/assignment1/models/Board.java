b06a55f90332466e39c15cef134682a75b263a3f
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-02 18:07:05 -0500

    I've written a mistake, since I'm not able to retrieve the number of tokens in the board with the same horizontal number.

--- app/src/main/java/comp3025/assignment1/models/Board.java
+++ app/src/main/java/comp3025/assignment1/models/Board.java
@@ -2,6 +2,7 @@ package comp3025.assignment1.models;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * This class is a board.
@@ -13,6 +14,19 @@ public class Board {
     private Map<SlotNumbers, Token> map = new HashMap<>();
 
     /**
+     * This method adds the token to the board.
+     * Only the horizontal number can be chosen.
+     * The board will choose the vertical number, depending on the number of tokens that have been added with the same horizontal number.
+     */
+    public void setToken(Token token, int horizontalNumber) {
+        Objects.requireNonNull(token);
+
+        //This has been a mistake.
+        //I need to change Map<SlotNumbers,Token>.
+        //I won't be able to retrieve how many existing tokens with the same horizontal number.
+    }
+
+    /**
      * This method retrieves the token with the provided slot numbers.
      * If no token has been added with the provided slot numbers, this method returns null.
      *
@@ -20,6 +34,8 @@ public class Board {
      * @return
      */
     public Token getToken(SlotNumbers slotNumbers) {
+        Objects.requireNonNull(slotNumbers);
+
         Token retrievedToken = this.map.get(slotNumbers);
 //The get method returns null if the slot numbers hasn't been added.
 
