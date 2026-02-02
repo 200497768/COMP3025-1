5110bb787da84769806e95e2586200f62e68420f
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-02 12:35:16 -0500

    I changed the Run class to RunModels.

--- app/src/main/java/comp3025/assignment1/models/RunModels.java
+++ app/src/main/java/comp3025/assignment1/models/RunModels.java
@@ -1,6 +1,6 @@
 package comp3025.assignment1.models;
 
-public class Run {
+public class RunModels {
     public static void main(String[] args) {
         //This method is used to determine whether I've written the models correctly.
         //It's not part of the assignment.
