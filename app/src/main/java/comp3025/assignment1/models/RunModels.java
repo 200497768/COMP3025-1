776a8a7b8116bd1cc9f03f5d422729f0daf93fef
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-02 17:38:46 -0500

    I've removed the RunModels class. I've determined that I can't run the main method this way. Instead, I need to write it as part of the MainActivity class, like how we did during the week 1 class.

--- app/src/main/java/comp3025/assignment1/models/RunModels.java
+++
@@ -1,9 +0,0 @@
-package comp3025.assignment1.models;
-
-public class RunModels {
-    public static void main(String[] args) {
-        //This method is used to determine whether I've written the models correctly.
-        //It's not part of the assignment.
-        System.out.println("Running");
-    }
-}
