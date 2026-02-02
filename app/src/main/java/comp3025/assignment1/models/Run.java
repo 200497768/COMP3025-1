3596a730f9611d71d26bbb7f9388bebb5f474bbc
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-02 12:34:10 -0500

    I added the main method, including text explaining that it's not part of the assignment.

---
+++ app/src/main/java/comp3025/assignment1/models/Run.java
@@ -0,0 +1,9 @@
+package comp3025.assignment1.models;
+
+public class Run {
+    public static void main(String[] args) {
+        //This method is used to determine whether I've written the models correctly.
+        //It's not part of the assignment.
+        System.out.println("Running");
+    }
+}
