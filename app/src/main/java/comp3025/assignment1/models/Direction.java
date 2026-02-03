2189b5454826e003259d72a324baa1710a8f2b15
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-03 14:40:57 -0500

    I added the 3 methods that create directions to the directions class, instead of the direction class. This way, the direction class will be the model.

--- app/src/main/java/comp3025/assignment1/models/Direction.java
+++ app/src/main/java/comp3025/assignment1/models/Direction.java
@@ -9,51 +9,5 @@ public class Direction {
         return 0;
     }
 
-    public static Direction getVerticalDirection() {
-        Direction verticalDirection = new Direction() {
-            @Override
-            public int getVerticalAddAmount() {
-                return 1;
-            }
 
-            @Override
-            public int getHorizontalAddAmount() {
-                return 0;
-            }
-        };
-
-        return verticalDirection;
-    }
-
-    public static Direction getHorizontalDirection() {
-        Direction horizontalDirection = new Direction() {
-            @Override
-            public int getVerticalAddAmount() {
-                return 0;
-            }
-
-            @Override
-            public int getHorizontalAddAmount() {
-                return 1;
-            }
-        };
-
-        return horizontalDirection;
-    }
-
-    public static Direction getDiagonalDirection() {
-        Direction diagonalDirection = new Direction() {
-            @Override
-            public int getVerticalAddAmount() {
-                return 1;
-            }
-
-            @Override
-            public int getHorizontalAddAmount() {
-                return 1;
-            }
-        };
-
-        return diagonalDirection;
-    }
 }
---
+++ app/src/main/java/comp3025/assignment1/models/Directions.java
@@ -0,0 +1,52 @@
+package comp3025.assignment1.models;
+
+public class Directions {
+
+    public static Direction getVerticalDirection() {
+        Direction verticalDirection = new Direction() {
+            @Override
+            public int getVerticalAddAmount() {
+                return 1;
+            }
+
+            @Override
+            public int getHorizontalAddAmount() {
+                return 0;
+            }
+        };
+
+        return verticalDirection;
+    }
+
+    public static Direction getHorizontalDirection() {
+        Direction horizontalDirection = new Direction() {
+            @Override
+            public int getVerticalAddAmount() {
+                return 0;
+            }
+
+            @Override
+            public int getHorizontalAddAmount() {
+                return 1;
+            }
+        };
+
+        return horizontalDirection;
+    }
+
+    public static Direction getDiagonalDirection() {
+        Direction diagonalDirection = new Direction() {
+            @Override
+            public int getVerticalAddAmount() {
+                return 1;
+            }
+
+            @Override
+            public int getHorizontalAddAmount() {
+                return 1;
+            }
+        };
+
+        return diagonalDirection;
+    }
+}
