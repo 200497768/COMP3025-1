19ff7571545d5fd9cad8e712630771507332fe18
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-03 14:14:44 -0500

    I added a method to the ordered token group class to add a token to the group.

--- app/src/main/java/comp3025/assignment1/models/OrderedTokenGroup.java
+++ app/src/main/java/comp3025/assignment1/models/OrderedTokenGroup.java
@@ -2,6 +2,7 @@ package comp3025.assignment1.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * An ordered token group is a vertical line in the board that can include multiple tokens.
@@ -23,7 +24,27 @@ public class OrderedTokenGroup {
         return token;
     }
 
+    /**
+     * This method returns the tokens from this ordered token group.
+     * In other words, this method returns tokens with the same vertical number.
+     *
+     * @return
+     */
     public List<Token> getTokens() {
         return tokens;
+    }
+
+    /**
+     * This method adds a token to this ordered token group.
+     * In other words, this method adds a token to this vertical line.
+     * This ordered token group will be responsible for determining the vertical number for the token that was added.
+     * The vertical number can't be chosen when using this method, since the token falls in the board.
+     *
+     * @param token
+     */
+    public void addToken(Token token) {
+        Objects.requireNonNull(token);
+
+        this.tokens.add(token);
     }
 }
