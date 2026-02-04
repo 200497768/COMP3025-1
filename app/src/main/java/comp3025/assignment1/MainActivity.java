85c1cab575a81a06eb725f57436af3d55503ff2c
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-04 17:24:48 -0500

    I changed the number for the vertical group to 0, instead of 1.

--- app/src/main/java/comp3025/assignment1/MainActivity.java
+++ app/src/main/java/comp3025/assignment1/MainActivity.java
@@ -42,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
         Board board = new Board(5, 4);
 
         Participant participant = new Participant("Hao Tian");
-        board.addToken(participant, 1);
-        board.addToken(participant, 1);
-        board.addToken(participant, 1);
-        board.addToken(participant, 1);
+        board.addToken(participant, 0);
+        board.addToken(participant, 0);
+        board.addToken(participant, 0);
+        board.addToken(participant, 0);
         Log.i("200497768", "Winner is " + board.getWinningParticipant());
 
 //As a student, I want to be able to see both of the views that are needed for this assignment.
