b34ba50ddc9d32535d6123b59d3a4006c81eab95
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-04 17:48:21 -0500

    I changed the vertical group class so that it uses tokenNumber instead of verticalNumber.

--- app/src/main/java/comp3025/assignment1/models/VerticalGroup.java
+++ app/src/main/java/comp3025/assignment1/models/VerticalGroup.java
@@ -20,26 +20,26 @@ public class VerticalGroup {
     private int capacity;
 
     /**
-     * This method returns a token from this ordered token group using the vertical number.
+     * This method returns a token from this vertical group.
      *
      * @param verticalNumber
      * @return
      */
-    public Token getToken(int verticalNumber) {
-        if (verticalNumber < 0) {
+    public Token getToken(int tokenNumber) {
+        if (tokenNumber < 0) {
             throw new IllegalArgumentException();
         }
 
-        if (verticalNumber >= this.tokens.size()) {
+        if (tokenNumber >= this.tokens.size()) {
             throw new IllegalArgumentException();
         }
 
-        Token token = this.tokens.get(verticalNumber);
+        Token token = this.tokens.get(tokenNumber);
         return token;
     }
 
     /**
-     * This method returns the tokens from this ordered token group.
+     * This method returns the tokens from this vertical group.
      * In other words, this method returns tokens with the same vertical number.
      *
      * @return
@@ -49,7 +49,7 @@ public class VerticalGroup {
     }
 
     /**
-     * This method returns whether a slot is still available in this ordered token group.
+     * This method returns whether a slot is still available in this vertical group.
      * If a slot is available, a token can be added using another method from this class.
      * If a slot isn't available, a token can't be added.
      *
@@ -60,7 +60,7 @@ public class VerticalGroup {
     }
 
     /**
-     * This method adds a token to this ordered token group.
+     * This method adds a token to this vertical group.
      * In other words, this method adds a token to this vertical line.
      * This ordered token group will be responsible for determining the vertical number for the token that was added.
      * The vertical number can't be chosen when using this method, since the token falls in the board.
