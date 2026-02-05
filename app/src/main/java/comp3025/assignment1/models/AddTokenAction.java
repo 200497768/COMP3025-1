a870dda18c26f4f11b0ffed8b079f353c741dd28
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-05 17:43:22 -0500

    I added the slot numbers field to the add token action class.

--- app/src/main/java/comp3025/assignment1/models/AddTokenAction.java
+++ app/src/main/java/comp3025/assignment1/models/AddTokenAction.java
@@ -4,4 +4,7 @@ package comp3025.assignment1.models;
  * This class is an action that can be used to add a token to a board.
  */
 public class AddTokenAction extends Action {
+
+    //This field is the slot numbers that the token needs to be added to.
+    private SlotNumbers slotNumbers;
 }
