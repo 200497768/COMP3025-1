c5901a939103497d7f7d1bc50e464e5c883f1550
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-05 17:41:31 -0500

    I created the action class, including the add token action class.

---
+++ app/src/main/java/comp3025/assignment1/models/Action.java
@@ -0,0 +1,8 @@
+package comp3025.assignment1.models;
+
+/**
+ * This class is an action that can happen to a board.
+ */
+public class Action {
+
+}
---
+++ app/src/main/java/comp3025/assignment1/models/AddTokenAction.java
@@ -0,0 +1,7 @@
+package comp3025.assignment1.models;
+
+/**
+ * This class is an action that can be used to add a token to a board.
+ */
+public class AddTokenAction extends Action {
+}
