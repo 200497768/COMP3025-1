3318de7636b82f8b832410bb91a0e018b138552c
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-05 15:23:47 -0500

    I added text that explains the slot numbers class.

--- app/src/main/java/comp3025/assignment1/models/SlotNumbers.java
+++ app/src/main/java/comp3025/assignment1/models/SlotNumbers.java
@@ -2,16 +2,20 @@ package comp3025.assignment1.models;
 
 /**
  * This class is a group of slot numbers.
- * A group of slot numbers is used to access a certain slot in a board.
- * A group of slot numbers includes a vertical slot number and a horizontal slot number.
- * The minimum slot number is 1.
- * The maximum slot number depends on the board.
- * The horizontal number determines the vertical group that's chosen.
- * The vertical number is determines the token that's chosen in a vertical group.
+ * A group of slot numbers is used to access a single slot in a board.
+ * A group of slot numbers includes a vertical group number and a token number.
+ * The minimum number is 0, and the maximum number depends on the board.
+ * The vertical group number determines the vertical group that's chosen.
+ * The token number is determines the token that's chosen in a vertical group.
  */
 public class SlotNumbers {
+
+    //This field is the vertical group number.
+    //The vertical group number is used to choose a single vertical group from the board.
     private int verticalGroupNumber;
 
+    //This field is the token number.
+    //The token number is used to choose a single token from a vertical group.
     private int tokenNumber;
 
     public SlotNumbers(int verticalGroupNumber, int tokenNumber) {
