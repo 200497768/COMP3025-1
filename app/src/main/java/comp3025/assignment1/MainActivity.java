b7ca070b8d82326b7d2b523e42170f8dd8dda8ad
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-05 14:55:05 -0500

    I removed a few lines that aren't needed.

--- app/src/main/java/comp3025/assignment1/MainActivity.java
+++ app/src/main/java/comp3025/assignment1/MainActivity.java
@@ -29,10 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //This method causes the code to run the welcome view.
-
         setContentView(R.layout.game);
 
-        System.out.println("onCreate method running.");
         Log.i("200497768", "onCreate method running.");
 
         LinearLayout boardArea = findViewById(R.id.boardArea);
