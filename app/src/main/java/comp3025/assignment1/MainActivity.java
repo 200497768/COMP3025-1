d5cef10fe83b07ca9c57c6106ac353fa0c1ce8ff
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-06 15:52:52 -0500

    I've started changing some of the code from the onCreate method that creates the board elements to the board area actions class.

--- app/src/main/java/comp3025/assignment1/MainActivity.java
+++ app/src/main/java/comp3025/assignment1/MainActivity.java
@@ -75,59 +75,6 @@ public class MainActivity extends AppCompatActivity {
             Log.i("200497768", line);
         }
 
-        //The vertical group number will be used to create a string that shows this number.
-        int verticalGroupNumber = 0;
-
-        //The board has been created, but the views still need to be added to the board area.
-        for (VerticalGroup verticalGroup : board.getVerticalGroups()) {
-
-            //Create an area for this vertical group, and add it to the board area.
-            LinearLayout verticalGroupArea=new LinearLayout(MainActivity.this);
-            verticalGroupArea.setOrientation(VERTICAL);
-            //APA for creating this is (Yadav, 2019)
-            int verticalGroupColor = Color.rgb(135,206,235);
-            verticalGroupArea.setBackgroundColor(verticalGroupColor);
-            //APA is SkyBlue.
-boardArea.addView(verticalGroupArea);
-
-            //Add a string to the vertical group area showing the number for this vertical group.
-            TextView verticalGroupNumberTextView = new TextView(MainActivity.this);
-            //APA for creating this view is (Yadav, 2019)
-            verticalGroupNumberTextView.setText("Vertical " + verticalGroupNumber + " ");
-            verticalGroupArea.addView(verticalGroupNumberTextView);
-
-//Go through all of the tokens in this vertical group.
-            //The token number needs to start with the maximum token number so that tokens are ordered how the board is supposed to be.
-            for (int tokenNumber = board.getVerticalGroupCapacity() - 1; tokenNumber >= 0; tokenNumber = tokenNumber - 1) {
-                //Create an element for this slot, depending on whether it has a token, or if it's empty.
-
-                //Retrieve the slot from this vertical group.
-                //The method from the board class is used, instead of the vertical group class, so that it can retrieve null.
-                //If the token was retrieved from the vertical group, the method would only allow accessing existing tokens.
-                SlotNumbers slotNumbers = new SlotNumbers(verticalGroupNumber, tokenNumber);
-                Token slot = board.getToken(slotNumbers);
-
-                if (slot == null) {
-                    //This slot is empty.
-                    TextView emptyTextView = new TextView(MainActivity.this);
-                    //APA for creating this view is (Yadav, 2019)
-                    emptyTextView.setText("Empty ");
-                    verticalGroupArea.addView(emptyTextView);
-                } else {
-                    //A token exists in this slot.
-                    TextView tokenTextView = new TextView(MainActivity.this);
-                    //APA for creating this view is (Yadav, 2019)
-                    tokenTextView.setText("Token ");
-                    tokenTextView.setBackgroundColor(participant.getColor());
-
-                    verticalGroupArea.addView(tokenTextView);
-                }
-            }
-
-            //Tokens and slots for this vertical group have been added.
-            //The vertical group number needs to be increased.
-            verticalGroupNumber = verticalGroupNumber + 1;
-        }
 
         //Show whether a participant has won.
         LinearLayout winningParticipantArea = findViewById(R.id.winningParticipantArea);
--- app/src/main/java/comp3025/assignment1/models/actions/BoardAreaActions.java
+++ app/src/main/java/comp3025/assignment1/models/actions/BoardAreaActions.java
@@ -1,8 +1,21 @@
 package comp3025.assignment1.models.actions;
 
-import android.widget.LinearLayout;
+import static android.widget.LinearLayout.VERTICAL;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import comp3025.assignment1.MainActivity;
+import comp3025.assignment1.R;
 import comp3025.assignment1.models.Board;
+import comp3025.assignment1.models.Participant;
+import comp3025.assignment1.models.SlotNumbers;
+import comp3025.assignment1.models.TextBoard;
+import comp3025.assignment1.models.Token;
+import comp3025.assignment1.models.VerticalGroup;
 
 /**
  * This class is responsible for changing elements when the board model changes.
@@ -10,15 +23,70 @@ import comp3025.assignment1.models.Board;
 public class BoardAreaActions extends Actions {
 
     private LinearLayout boardArea;
+    private Context context;
 
-    public BoardAreaActions(Board board, LinearLayout boardArea) {
+    public BoardAreaActions(Board board, LinearLayout boardArea, Context context) {
         super(board);
         this.boardArea = boardArea;
+        this.context = context;
     }
 
     @Override
     public void boardCreated() {
+        //The vertical group number will be used to create a string that shows this number.
+        int verticalGroupNumber = 0;
 
+        //The board has been created, but the views still need to be added to the board area.
+        for (VerticalGroup verticalGroup : this.board.getVerticalGroups()) {
+
+            //Create an area for this vertical group, and add it to the board area.
+            LinearLayout verticalGroupArea = new LinearLayout(this.context);
+            verticalGroupArea.setOrientation(VERTICAL);
+            //APA for creating this is (Yadav, 2019)
+            int verticalGroupColor = Color.rgb(135, 206, 235);
+            verticalGroupArea.setBackgroundColor(verticalGroupColor);
+            //APA is SkyBlue.
+            boardArea.addView(verticalGroupArea);
+
+            //Add a string to the vertical group area showing the number for this vertical group.
+            TextView verticalGroupNumberTextView = new TextView(this.context));
+            //APA for creating this view is (Yadav, 2019)
+            verticalGroupNumberTextView.setText("Vertical " + verticalGroupNumber + " ");
+            verticalGroupArea.addView(verticalGroupNumberTextView);
+
+//Go through all of the tokens in this vertical group.
+            //The token number needs to start with the maximum token number so that tokens are ordered how the board is supposed to be.
+            for (int tokenNumber = this.board.getVerticalGroupCapacity() - 1; tokenNumber >= 0; tokenNumber = tokenNumber - 1) {
+                //Create an element for this slot, depending on whether it has a token, or if it's empty.
+
+                //Retrieve the slot from this vertical group.
+                //The method from the board class is used, instead of the vertical group class, so that it can retrieve null.
+                //If the token was retrieved from the vertical group, the method would only allow accessing existing tokens.
+                SlotNumbers slotNumbers = new SlotNumbers(verticalGroupNumber, tokenNumber);
+                Token slot = this.board.getToken(slotNumbers);
+
+                if (slot == null) {
+                    //This slot is empty.
+                    TextView emptyTextView = new TextView(this.context));
+                    //APA for creating this view is (Yadav, 2019)
+                    emptyTextView.setText("Empty ");
+                    verticalGroupArea.addView(emptyTextView);
+                } else {
+                    //A token exists in this slot.
+                    TextView tokenTextView = new TextView(this.context));
+                    //APA for creating this view is (Yadav, 2019)
+                    tokenTextView.setText("Token ");
+                    Participant participant = new Participant("Hao Tian");
+                    tokenTextView.setBackgroundColor(participant.getColor());
+
+                    verticalGroupArea.addView(tokenTextView);
+                }
+            }
+
+            //Tokens and slots for this vertical group have been added.
+            //The vertical group number needs to be increased.
+            verticalGroupNumber = verticalGroupNumber + 1;
+        }
     }
 
     @Override
