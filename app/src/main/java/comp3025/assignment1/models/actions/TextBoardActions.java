3a3eaff2c50b491c467d5c2943a3694cd59e050d
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-06 15:56:38 -0500

    I created the text board actions class. This class will be used to show the text board when the board model changes.

---
+++ app/src/main/java/comp3025/assignment1/models/actions/TextBoardActions.java
@@ -0,0 +1,7 @@
+package comp3025.assignment1.models.actions;
+
+/**
+ * This class is similar to the board area actions class, but it uses the text board instead.
+ */
+public class TextBoardActions {
+}
