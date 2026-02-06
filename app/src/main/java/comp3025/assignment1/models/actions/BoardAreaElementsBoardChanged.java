50c012968b712f861cec5ca08d80691fc456db11
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-06 13:35:26 -0500

    I created a class that will be used to change elements from the board area when the board model changes.

---
+++ app/src/main/java/comp3025/assignment1/models/actions/BoardAreaElementsBoardChanged.java
@@ -0,0 +1,8 @@
+package comp3025.assignment1.models.actions;
+
+/**
+ * This class is responsible for changing elements when the board model changes.
+ */
+public class BoardAreaElementsBoardChanged extends BoardChanged {
+
+}
