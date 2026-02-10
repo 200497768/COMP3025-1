c3cc8ccaaf0251bcfb640249119c4d8d24aaf04d
200497768 <169184214+200497768@users.noreply.github.com>
2026-02-10 12:51:39 -0500

    I removed the onCreate method lines that aren't needed for CompletedActivity.

--- app/src/main/java/comp3025/assignment1/CompletedActivity.java
+++ app/src/main/java/comp3025/assignment1/CompletedActivity.java
@@ -4,11 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 /**
  * This class shows the score for every participant in this competition.
@@ -22,13 +18,9 @@ public class CompletedActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_completed);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 
     /**
